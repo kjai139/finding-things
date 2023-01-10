21e45afd3a642b6f238b487795d592adaf2d4ddb
Simon Y <therealkjai139@gmail.com>
2023-01-09 19:56:37 -0500

    homepage started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import { FireLogin } from './Components/FireLogin';
+import { Homepage } from './Components/Homepage';
 import { TopNav } from './Components/TopNav';
 
 function App() {
   return (
     <div className="App">
       <TopNav />
+      <Homepage />
     </div>
   );
 }
---
+++ src/Components/Homepage.js
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import n64stage from "./assets/maps/n64_stage.jpg"
+import dreamcastStage from "./assets/maps/dreamcast_stage.jpg"
+import ps2Stage from "./assets/maps/ps2_stage.jpg"
+
+
+const Homepage = () => {
+
+    const [stages, setStages] = useState([{
+        stageTitle: 'N64',
+        stageImg: n64stage,
+        uuid: 'e08399e9-6e89-40cc-bca1-6b58651895d6'
+    }, {
+        stageTitle: 'Dreamcast',
+        stageImg: dreamcastStage,
+        uuid: '5b633e6c-3059-424f-9ee8-3a8cf02299a3'
+    }, {
+        stageTitle: 'PS2',
+        stageImg: ps2Stage,
+        uuid: '3f1de7b1-1085-4527-98ac-a2d0030af5b3'
+    }])
+
+    const renderStages = () => {
+        return stages.map(value =>
+            <div className="stageGridCard" key={`stages${value.uuid}`}>
+                <div className="stagesImg" key={`img${value.uuid}`} style = {{
+                    backgroundImage: `${value.stageImg ? `url(${value.stageImg})` : '' }`
+                }}></div>
+                <span className="stagesName" key={`stagesName${value.uuid}`}>{value.stageTitle ? value.stageTitle : 'Loading...'}</span>
+
+            </div>
+            )
+    }
+
+    return (
+        <div className="selectionGrid">
+            {renderStages()}
+        </div>
+    )
+}
+
+export {Homepage}
